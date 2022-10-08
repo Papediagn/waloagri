43a637ac8bb4ef7d0665bd4bd35251425cf8808d
Papediagn <diagne.papeibrahima@gmail.com>
2022-10-08 17:12:27 +0100

    deploiement

--- sent.php
+++
@@ -1,7 +0,0 @@
-<?php
-$nom=$_POST['nom'];
-$email=$_POST['email'];
-$message=$_POST['message'];
-mail("diagne.papeibrahima@gmail.com",$email,$nom,$message);
-header("location:index.html?s=ok");
-?>
